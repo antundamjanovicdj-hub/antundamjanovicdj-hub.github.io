32a8533babcbeaf85530116807d7b94ad1d378b1
antundamjanovicdj-hub <antun.damjanovicdj@gmail.com>
2025-12-19 15:23:42 +0100

    Update platform.js

--- core/platform.js
+++ core/platform.js
@@ -1,1 +1,9 @@
-
+// core/platform.js
+export function getPlatformFlags() {
+  const ua = navigator.userAgent;
+  return {
+    isIOS: /iPad|iPhone|iPod/.test(ua),
+    isSamsung: /Samsung/i.test(ua),
+    isPixel: /Pixel/i.test(ua),
+  };
+}

9701bdb635ac005e4331885e673f9c38cd238d47
antundamjanovicdj-hub <antun.damjanovicdj@gmail.com>
2025-12-19 18:36:56 +0100

    Update tasks.calendar.js

--- modules/tasks/tasks.calendar.js
+++ modules/tasks/tasks.calendar.js
@@ -1,28 +1,49 @@
 // modules/tasks/tasks.calendar.js
-export function exportToCalendar({ task, lang, T, platform }) {
+export function exportToCalendar({
+  task,
+  lang,
+  T,
+  platform,
+  cancel = false
+}) {
   const start = new Date(task.date + "T" + task.time);
   if (isNaN(start)) return;
 
   const end = new Date(start.getTime() + 30 * 60000);
   const uid = `lifekompas-${task.id}@lifekompas`;
-  const dt = d => d.toISOString().replace(/[-:]/g, "").split(".")[0] + "Z";
+
+  const dt = d =>
+    d.toISOString().replace(/[-:]/g, "").split(".")[0] + "Z";
 
   let method = "PUBLISH";
   let sequenceLine = "";
 
-  if (platform.isIOS) {
+  if (platform.isIOS || platform.isPixel) {
     method = "REQUEST";
-    sequenceLine = `SEQUENCE:${task.seq}\r\n`;
-  } else if (platform.isPixel) {
     sequenceLine = `SEQUENCE:${task.seq}\r\n`;
   }
 
-  const alarm = task.reminder > 0
-    ? `BEGIN:VALARM\r\nTRIGGER:-PT${task.reminder}M\r\nACTION:DISPLAY\r\nDESCRIPTION:${task.title}\r\nEND:VALARM\r\n`
-    : "";
+  const alarm =
+    !cancel && task.reminder > 0
+      ? `BEGIN:VALARM\r\nTRIGGER:-PT${task.reminder}M\r\nACTION:DISPLAY\r\nDESCRIPTION:${task.title}\r\nEND:VALARM\r\n`
+      : "";
+
+  const statusLine = cancel ? "STATUS:CANCELLED\r\n" : "";
 
   const ics =
-`BEGIN:VCALENDAR\r\nVERSION:2.0\r\nMETHOD:${method}\r\nPRODID:-//LifeKompas//HR\r\nBEGIN:VEVENT\r\nUID:${uid}\r\n${sequenceLine}DTSTAMP:${dt(new Date())}\r\nSUMMARY:${task.title}\r\nDTSTART:${dt(start)}\r\nDTEND:${dt(end)}\r\nDESCRIPTION:${T[lang].calendarNote}\r\n${alarm}END:VEVENT\r\nEND:VCALENDAR`;
+`BEGIN:VCALENDAR\r
+VERSION:2.0\r
+METHOD:${method}\r
+PRODID:-//LifeKompas//HR\r
+BEGIN:VEVENT\r
+UID:${uid}\r
+${sequenceLine}DTSTAMP:${dt(new Date())}\r
+DTSTART:${dt(start)}\r
+DTEND:${dt(end)}\r
+SUMMARY:${task.title}\r
+DESCRIPTION:${T[lang].calendarNote}\r
+${statusLine}${alarm}END:VEVENT\r
+END:VCALENDAR`;
 
   const blob = new Blob([ics], { type: "text/calendar;charset=utf-8" });
   const url = URL.createObjectURL(blob);
@@ -35,4 +56,3 @@ export function exportToCalendar({ task, lang, T, platform }) {
   document.body.removeChild(a);
   URL.revokeObjectURL(url);
 }
-
